89bd5116dd1fd0c98419e85c654ef0a5d5c4a1f2
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-10 01:45:20 -0300

    Adicionando componente Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import cover from '../../assets/Cover.png'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 296px;
+  background: url(${cover}) no-repeat center;
+  background-size: cover;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
--- src/layouts/DefultLayout/index.tsx
+++ src/layouts/DefultLayout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import { LayoutContainer } from './styles'
+import { Header } from '../../components/Header'
 
 export function DeafultLayout() {
   return (
     <LayoutContainer>
+      <Header />
       <Outlet />
     </LayoutContainer>
   )
