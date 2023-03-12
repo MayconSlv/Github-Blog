6b6b184476e3735198461786741d636e48273bfb
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-12 19:16:28 -0300

    Iniciando requisição GET a API do github

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,38 @@
+import { useCallback, useEffect, useState } from "react";
 import { Post } from "./componentes/Post";
 import { Profile } from "./componentes/Profile";
 import { SearchArea } from "./componentes/SearchArea";
 import { PostListContainer } from "./styles";
+import { api } from "../../lib/axios";
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
+
+interface IPost {
+  title: string
+}
 
 export function Blog() {
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  const getPosts = useCallback(
+    async (query: string = "") => {
+      try {
+        const response = await api.get(
+          `/search/issues?q=${query}%20repo:${username}/${reponame}`
+          )
+          console.log(posts)
+          console.log(response.data)
+          setPosts(response.data.items)
+      } finally {
+
+      }
+  }, [posts])
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
       <Profile />
