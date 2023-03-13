8f514e61db9d82fa7fb50fc5285ab0d51e8ce2e1
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-13 01:40:12 -0300

    Criando schema com react hook form

--- src/pages/blog/componentes/SearchArea/index.tsx
+++ src/pages/blog/componentes/SearchArea/index.tsx
@@ -1,13 +1,30 @@
-import { SearchAreaForm } from "./styles";
+import { SearchAreaForm } from "./styles"
+import * as z from 'zod'
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormAreaSchema = z.object({
+  query: z.string()
+})
+
+type SearchFormInput = z.infer<typeof searchFormAreaSchema>
 
 export function SearchArea() {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormAreaSchema)
+  })
+
+  function handleSearchPost (data: SearchFormInput) {
+    console.log(data)
+  }
+
   return (
-    <SearchAreaForm>
+    <SearchAreaForm onSubmit={handleSubmit(handleSearchPost)}>
       <header>
         <h3>Publicações</h3>
         <span>6 publicações</span>
       </header>
-      <input type="text" placeholder="Buscar conteúdo"/>
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
     </SearchAreaForm>
   )
 }
