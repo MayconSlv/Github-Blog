bdd5ec62c889e87a9fd2effabf1aea70043af9df
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-13 19:11:52 -0300

    Integrando API a página Post

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,6 +1,33 @@
+import { useCallback, useEffect, useState } from "react";
 import { PostInfo } from "./components/PostInfo";
+import { api } from "../../lib/axios";
+import { IPost } from "../blog";
+import { useParams } from "react-router-dom";
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
 
 export function Post() {
+  const [posts, setPosts] = useState({} as IPost)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const { id } = useParams()
+
+  const getPostDetails = useCallback(async () => {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(`/repos/${username}/${reponame}/issues/${id}`)
+      setPosts(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    getPostDetails()
+  }, [])
+
   return (
     <>
       <PostInfo />
