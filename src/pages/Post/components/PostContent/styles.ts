95b5ab05203b947494122d634a93f65e5f8aaf57
Maycon Silva <mayconslv.contato@gmail.com>
2023-03-23 16:11:07 -0300

    Estilizando textos markdown

--- src/pages/Post/components/PostContent/styles.ts
+++ src/pages/Post/components/PostContent/styles.ts
@@ -6,4 +6,12 @@ export const PostContentContainer = styled.section`
   gap: 1.6rem;
   padding: 4rem 3.2rem;
   margin-bottom: 12.8rem;
+
+  h1, h2, h3 {
+    color: ${({theme}) => theme.colors['brand-blue']};
+  }
+
+  pre {
+    width: 100%;
+  }
 `
